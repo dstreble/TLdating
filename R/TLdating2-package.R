fe5e6157b95450dcd7f65d250900695a0a6b1627
David <david.strebler@uni-koeln.de>
2016-02-16 14:35:36 +0100

    Creation of master branch

---
+++ R/TLdating2-package.R
@@ -0,0 +1,58 @@
+#' Tools for thermoluminescences dating
+#'
+#' A series of functions for thermoluminescence dating using the MAAD or the
+#' SAR protocol. This package adds to the R package "luminescence".
+#'
+#' \tabular{ll}{  Package: \tab TLdating2 \cr
+#'                Type: \tab Package\cr
+#'                Version: \tab 0.0.1 [upcoming] \cr
+#'                Date: \tab 2015-05-XX \cr
+#'                License: \tab GPL-3
+#'             }
+#'
+#' @name TLdating-package
+#' @aliases TLdating2-package TLdating2
+#' @docType package
+#'
+#' @author
+#'  \bold{Authors} \cr
+#'  David Strebler, University of Cologne, Germany
+#'
+#'  \bold{Beta-tester} \cr
+#'  Anja Zander, University of Cologne, Germany
+#'
+##  \bold{Supervisor} \cr
+##  ... \cr
+#'
+##  \bold{Support contact} \cr
+##  ... \cr
+#'
+##  \bold{Bug reporting} \cr
+##  ... \cr
+#'
+##  \bold{Project website} \cr
+##  ... \cr
+#'
+##  \bold{Project source code repository} \cr
+##  ... \cr
+#'
+#'  \bold{Related package projects}\cr
+#'    \url{http://www.r-luminescence.de} \cr
+#'    \url{http://cran.r-project.org/web/packages/Luminescence}\cr
+#'    \url{http://cran.r-project.org/web/packages/RLumShiny}\cr
+#'
+#'  \bold{Package maintainer}
+#'  David Strebler, Geographisches Institut, Universitat zu Koeln, Cologne, Germany. \cr
+#'  \email{david.strebler@uni-koeln.de}
+#'
+##  \bold{Acknowledgement}
+##  ... \cr
+#'
+#'
+#' @keywords package
+#'
+#' @import Luminescence gplots methods
+
+NULL
+
+
